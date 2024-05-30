e0602f1365002f64982c3cd10d834782946ba0f6
trevorsworld <trevors.karithi@student.moringaschool.com>
2024-05-30 18:27:58 +0300

    completed assignments

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+     with open(f'{file_name}.txt', mode='w') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode='a') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+     with open(f'{file_name}.txt', 'r') as file:
+        return file.read()
+
+
+
+file_name = "example"
+file_content = "This is the content of the file."
+
+write_file(file_name, file_content)
